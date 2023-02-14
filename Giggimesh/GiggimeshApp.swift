40ffb29d2fe536f473e68af4808eadf7e51d6fde
BrotherGrilka <exweerdo@gmail.com>
2023-02-14 07:22:12 -0500

    Initial Commit

---
+++ Giggimesh/GiggimeshApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiggimeshApp.swift
+//  Giggimesh
+//
+//  Created by Duncan Davidson on 2/14/23.
+//
+
+import SwiftUI
+
+@main
+struct GiggimeshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
